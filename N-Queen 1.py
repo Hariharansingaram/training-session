e2a198f3fc624b1dfe2561c23ef6719eaece0bc6
Hariharansingaram <52225820+Hariharansingaram@users.noreply.github.com>
2020-02-20 13:18:21 +0530

    Create N-Queen 1.py

---
+++ N-Queen 1.py
@@ -0,0 +1,15 @@
+#question
+N - Queens problem is to place n - queens in such a manner on an n x n chessboard that no queens attack each other by being in the same row, column or diagonal.
+Input Format
+One integer input
+Constraints
+1<=n<=10
+Output Format
+The matrix of 0 and 1, where 1 represents the position of the queen and 0 represents the empty space.
+Sample Input 0
+4
+Sample Output 0
+0 1 0 0
+0 0 0 1
+1 0 0 0
+0 0 1 0
