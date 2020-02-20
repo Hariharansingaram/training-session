99473f1d7905503ed3829d76643e8f5ccd7de787
Hariharansingaram <52225820+Hariharansingaram@users.noreply.github.com>
2020-02-20 11:40:27 +0530

    Create Add till Unity.py

---
+++ Add till Unity.py
@@ -0,0 +1,28 @@
+n=int(input())
+if(n%9):
+    print(n%9)
+else:
+    print("9")
+
+
+
+Given a number N, keep finding the digital sum of the number until it becomes a single digit number.
+
+Input Format
+
+One single Integer N
+
+Constraints
+
+1<=N<=10^18
+
+Output Format
+
+One single digit number
+
+Sample Input 0
+
+4129984
+Sample Output 0
+
+1
