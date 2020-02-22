f79f2e5f048753b60b25e8d5531171bed0339626
Hariharansingaram <52225820+Hariharansingaram@users.noreply.github.com>
2020-02-22 11:13:09 +0530

    Create GM Pattern.py

---
+++ GM Pattern.py
@@ -0,0 +1,15 @@
+n=5
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if(i==0 or j==0 or i==n-1 and j<m or j==n-1 and i>m or j==m and i>=m or i==m and j>m):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print(" ",end="")
+    for j in range(n):
+        if(j==0 or j==n-1 or i==j and j<=m or i+j==n-1 and i<m):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
