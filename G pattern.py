27f171518c7091022766803e791423f06cbc3bd9
Hariharansingaram <52225820+Hariharansingaram@users.noreply.github.com>
2020-02-22 10:47:21 +0530

    Create G pattern.py

---
+++ G pattern.py
@@ -0,0 +1,9 @@
+n=5
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if(i==0 or j==0 or i==n-1 and j<m or j==n-1 and i>m or j==m and i>=m or i==m and j>m):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
